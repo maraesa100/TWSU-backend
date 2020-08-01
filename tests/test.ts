40ab3edc34cf0600e7b98538f1d4fbd018b1b6bb
Siobhan McKenzie <siobhan@Siobhans-MacBook-Pro.local>
2020-08-01 12:48:01 +0100

    basic test setup

--- tests/test.ts
+++ tests/test.ts
@@ -3,8 +3,22 @@ import { expect } from 'chai'
 import 'mocha'
 
 describe('filterFunction', () => {
-  it('should return a percentage or null', () => {
-    const result = filterFunction('testing')
+  it('should return with 200 - or 200% more happy words than sad words', () => {
+    const result = filterFunction(
+      'I am feeling sad, but I am delighted at this delightful meal'
+    )
+    expect(result).to.equal(200)
+  })
+
+  it('should return with unknown', () => {
+    const result = filterFunction('I am delighte. This is a spelling error')
     expect(result).to.equal(null)
+  })
+
+  it('should return with 100', () => {
+    const result = filterFunction(
+      'I am delight. I am joy. I am not miserable or sad. '
+    )
+    expect(result).to.equal(100)
   })
 })
